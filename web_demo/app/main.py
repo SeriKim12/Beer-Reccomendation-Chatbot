75593e76304814b8acb6368a72565b6cfcf6e101
SeriKim12 <105254198+SeriKim12@users.noreply.github.com>
2022-06-20 03:21:47 +0900

    Update main.py

--- web_demo/app/main.py
+++ web_demo/app/main.py
@@ -171,129 +171,29 @@ def get_bot_response():
     return message
 
 
-  # elif 'type' and 'abv' and 'flavor' and 'taste' not in inferred_tags[0] : #len(set(inferred_tags))==0: #filled_slot:
-  #   message = '원하는 맥주에 대해 알려줘~~ 종류는? 도수는? 향은? 맛은? 어떤 게 좋아??~?~~~'
-
   elif 'type' in inferred_tags[0] and 'abv' not in inferred_tags[0] and 'flavor' not in inferred_tags[0] and 'taste' not in inferred_tags[0]:
     message = '접수 완료! 이제 원하는 도수, 향, 맛에 대해서도 알려줘'
+    if 'abv' in inferred_tags[0] and 'flavor' in inferred_tags[0] and 'taste' in inferred_tags[0]:
+      message = '네게 딱 맞을 맥주를 찾고 있는 중...'
     return message
 
-  # elif '종류' in filled_slot and '도수' in empty_slot and '향' in empty_slot and '맛'in empty_slot:
-  #   message = '접수 완료! 이제 원하는 도수, 향, 맛에 대해서도 알려줘'
-  #   return message
+  elif 'abv' in inferred_tags[0] and 'type' not in inferred_tags[0] and 'flavor' not in inferred_tags[0] and 'taste' not in inferred_tags[0]:
+    message = '접수 완료! 이제 원하는 종류, 향, 맛에 대해서도 알려줘'
+    if 'type' in inferred_tags[0] and 'flavor' in inferred_tags[0] and 'taste' in inferred_tags[0]:
+      message = '네게 딱 맞을 맥주를 찾고 있는 중...'
+    return message
 
-  #elif len(list(set(inferred_tags)))==1: inferred_tags의 유니크 값을 확인하려고 set으로 감싸고 길이 알아보려고
-  # 리스트로 감쌌는데 TypeError: unhashable type: 'list' 라고 에러가 나서 포기함
-  # elif ('type' in inferred_tags[0]) and ('abv' and 'flavor' and 'taste' not in inferred_tags[0]):
-  # elif (app.slot_dict['type'] is not None) and (app.slot_dict['abv'] is None and 
-  # app.slot_dict['flavor'] is None and app.slot_dict['taste'] is None):
-  #   message = '접수 완료! 이제 원하는 도수, 향, 맛에 대해서도 알려줘'
+  elif 'flavor' in inferred_tags[0] and 'abv' not in inferred_tags[0] and 'type' not in inferred_tags[0] and 'taste' not in inferred_tags[0]:
+    message = '접수 완료! 이제 원하는 종류, 도수, 맛에 대해서도 알려줘'
+    if 'abv' in inferred_tags[0] and 'type' in inferred_tags[0] and 'taste' in inferred_tags[0]:
+      message = '네게 딱 맞을 맥주를 찾고 있는 중...'
+    return message
 
-  # elif ('abv' in inferred_tags[0]) and ('type' and 'flavor' and 'taste' not in inferred_tags[0]):
-  #   message = '접수 완료! 이제 원하는 종류, 향, 맛에 대해서도 알려줘'
-
-  # elif ('flavor' in inferred_tags[0]) and ('abv' and 'type' and 'taste' not in inferred_tags[0]):
-  #   message = '접수 완료! 이제 원하는 종류, 도수, 맛에 대해서도 알려줘'
-    
-  # elif ('taste' in inferred_tags[0]) and ('abv' and 'flavor' and 'type' not in inferred_tags[0]):
-  #   message = '접수 완료! 이제 원하는 종류, 도수, 향에 대해서도 알려줘'
-
-  # return message
-
-# def catch_slot(i, inferred_tags, text_arr, slot_text):
-#   if not inferred_tags[0][i] == "O":
-#     word_piece = re.sub("_", " ", text_arr[i])
-#     if word_piece == 'ᆫ':
-#       word = slot_text[inferred_tags[0][i]]
-#       slot_text[inferred_tags[0][i]] = word[:-1]+chr(ord(word[-1])+4)
-#     else:    
-#       slot_text[inferred_tags[0][i]] += word_piece
-
-
-
-
-  # elif ['종류'] in filled_slot:
-  #   if ['도수', '향', '맛'] in empty_slot:
-  #     message = f'접수 완료! 이제 원하는 도수, 향, 맛에 대해서도 알려줘'
-  #   elif ['도수'] in filled_slot and ['향', '맛'] in empty_slot:
-  #     message = '오케이! 원하는 향과 맛은?'    
-  #   elif ['향'] in filled_slot and ['도수', '맛'] in empty_slot:
-  #     message = '오케이! 원하는 도수와 맛은?'
-  #   elif ['맛'] in filled_slot and ['도수', '향'] in empty_slot:
-  #     message = '오케이! 원하는 도수와 향은?'
-  #   elif ['도수', '향'] in filled_slot and ['맛'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 맛은?'    
-  #   elif ['도수', '맛'] in filled_slot and ['향'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 향은?'
-  #   elif ['향', '맛'] in filled_slot and ['도수'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 도수는?'
-
-
-  # elif ['도수'] in filled_slot:
-  #   if ['종류', '향', '맛'] in empty_slot:
-  #     message = f'접수 완료! 이제 원하는 종류, 향, 맛에 대해서도 알려줘'
-  #   elif ['종류'] in filled_slot and ['향', '맛'] in empty_slot:
-  #     message = '오케이! 원하는 향과 맛은?'    
-  #   elif ['향'] in filled_slot and ['종류', '맛'] in empty_slot:
-  #     message = '오케이! 원하는 종류와 맛은?'
-  #   elif ['맛'] in filled_slot and ['종류', '향'] in empty_slot:
-  #     message = '오케이! 원하는 종류와 향은?'
-  #   elif ['종류', '향'] in filled_slot and ['맛'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 맛은?'    
-  #   elif ['종류', '맛'] in filled_slot and ['향'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 향?'
-  #   elif ['맛', '향'] in filled_slot and ['종류'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 종류는?'
-
-
-  # elif ['향'] in filled_slot:
-  #   if ['종류', '도수', '맛'] in empty_slot:
-  #     message = f'접수 완료! 이제 원하는 종류, 도수, 맛에 대해서도 알려줘'
-  #   elif ['종류'] in filled_slot and ['도수', '맛'] in empty_slot:
-  #     message = '오케이! 원하는 도수와 맛은?'    
-  #   elif ['도수'] in filled_slot and ['종류', '맛'] in empty_slot:
-  #     message = '오케이! 원하는 종류와 맛은?'
-  #   elif ['맛'] in filled_slot and ['종류', '도수'] in empty_slot:
-  #     message = '오케이! 원하는 종류와 도수는?'
-  #   elif ['종류', '도수'] in filled_slot and ['맛'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 맛은?'    
-  #   elif ['종류', '맛'] in filled_slot and ['도수'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 도수는?'
-  #   elif ['도수', '맛'] in filled_slot and ['종류'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 종류는?'
-
-
-  # elif ['맛'] in filled_slot:
-  #   if ['종류', '도수', '향'] in empty_slot:
-  #     message = f'접수 완료! 이제 원하는 종류, 도수, 향에 대해서도 알려줘'
-  #   elif ['종류'] in filled_slot and ['도수', '향'] in empty_slot:
-  #     message = '오케이! 원하는 도수와 향은?'    
-  #   elif ['도수'] in filled_slot and ['종류', '향'] in empty_slot:
-  #     message = '오케이! 원하는 종류와 향은?'
-  #   elif ['향'] in filled_slot and ['종류', '도수'] in empty_slot:
-  #     message = '오케이! 원하는 종류와 도수는?'
-  #   elif ['종류', '도수'] in filled_slot and ['향'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 향은?'    
-  #   elif ['종류', '향'] in filled_slot and ['도수'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 도수는?'
-  #   elif ['도수', '향'] in filled_slot and ['종류'] in empty_slot:
-  #     message = '좋아 이제 마지막으로 원하는 종류는?'
-
-
-  # elif ['종류', '도수', '향', '맛'] in filled_slot:
-  #   message = '접수 완료!! 네게 딱 맞는 맥주를 찾고 있는 중...'
-
-  # if userText in ['quit', '종료', '그만', '멈춰', 'stop', '안마실래', '싫어', '안해', 'go away']:
-  #   message = 'Okay bye...'
-
-  # userText 안에 endings 문자가 들어있으면 message = Okay bye
-#   endings = ['quit', '종료', '그만', '멈춰', 'stop', '안마실래', '싫어', '안해', 'go away']
-#   #if [txt for txt in endings if txt in userText] is not None: 안녕의 '안'도 엔딩으로 잡아서 폐기
-#   if userText in endings:
-#     message = 'Okay bye...'
-#     #home(app)
-
-
+  elif 'taste' in inferred_tags[0] and 'abv' not in inferred_tags[0] and 'flavor' not in inferred_tags[0] and 'type' not in inferred_tags[0]:
+    message = '접수 완료! 이제 원하는 종류, 도수, 향에 대해서도 알려줘'
+    if 'abv' in inferred_tags[0] and 'flavor' in inferred_tags[0] and 'type' in inferred_tags[0]:
+      message = '네게 딱 맞을 맥주를 찾고 있는 중...'
+    return message
   
 
 
