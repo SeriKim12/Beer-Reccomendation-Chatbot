de814e2e77c82734810fc898f4fcf56822608526
Pjh940114 <99070332+Pjh940114@users.noreply.github.com>
2022-06-21 17:57:29 +0900

    Update main_jh.py

--- web_demo/app/main_jh.py
+++ web_demo/app/main_jh.py
@@ -20,11 +20,8 @@ bert_model_hub_path = '/content/drive/MyDrive/codes/bert-module'
 is_bert = True
 
 vocab_file = os.path.join(bert_model_hub_path, "assets/vocab.korean.rawtext.list")
-# 토큰화된 단어들에 숫자 매겨놓은 것
 bert_to_array = BERTToArray(is_bert, vocab_file)
-# 벡터라이저
 tokenizer = FullTokenizer(vocab_file=vocab_file)
-# 보캡 파일로 토크나이징 하는 도구?
 
 load_folder_path = '/content/drive/MyDrive/codes/save_model'
 # loading models
@@ -46,8 +43,6 @@ sess = tf.compat.v1.Session(config=config)
 graph = tf.compat.v1.get_default_graph()
 
 model = BertSlotModel.load(load_folder_path, sess)
-# finetuned_epoch128로 훈련시킨 슬롯태깅모델 (?)
-
 
 types = ['에일', 'IPA', '라거', '바이젠', '흑맥주']
 abv = ['3도', '4도', '5도', '6도', '7도', '8도',
@@ -63,7 +58,12 @@ taste = ['단', '달달한', '달콤한', '안단', '안 단',
               '쓰지않은/','안신', '안 신', '과일', '고소한', '구수한']
 
 options = {'types':'종류', 'abv':'도수', 'flavor':'향', 'taste':'맛'}
-dic = {i:globals()[i] for i in options}
+# dic = {i:globals()[i] for i in options}
+dic = {'types': ['에일', 'IPA', '라거', '바이젠', '흑맥주'],
+'abv': ['3도', '4도', '5도', '6도', '7도', '8도', '3도이상', '4도이상', '5도이상', '6도이상', '7도이상', '3도 이상', '4도 이상', '5도 이상', '6도 이상', '7도 이상', '4도이하', '5도이하', '6도이하', '7도이하', '8도이하', '4도 이하', '5도 이하', '6도 이하', '7도 이하', '8도 이하'],
+'flavor': ['과일', '홉', '꽃', '상큼한', '커피', '스모키한'],
+'taste': ['단', '달달한', '달콤한', '안단', '안 단', '달지 않은', '달지않은', '쓴', '씁쓸한', '쌉쌀한', '달콤씁쓸한', '안쓴', '안 쓴', '쓰지 않은', '신', '상큼한', '새콤달콤한', '시지 않은', '시지않은', '쓰지않은', '안신', '안 신', '과일',
+'고소한', '구수한']}
 # globals()[원하는 변수 이름] = 변수에 할당할 값 : 변수 여러개 동시 생성
 # dic = {'types': types, 'abv': abv, 'flavor': flavor, 'taste': taste}
 
@@ -74,7 +74,7 @@ cmds = {'명령어':[],
         '맛':taste}
 cmds["명령어"] = [k for k in cmds]
 # cmds["명령어"] = ['명령어', '종류', '도수', '향', '맛']
-tag_list = ['종류', '도수', '향', '맛']
+# tag_list = ['종류', '도수', '향', '맛']
 
 
 app = Flask(__name__)
@@ -130,6 +130,7 @@ def get_bot_response():
     #if slots_score[i] >= app.score_limit:      
     if slots_score[0][i] >= app.score_limit:
       catch_slot(i, inferred_tags, text_arr, slot_text)
+      # print("슬롯태깅 :", slot_text)
       #태그가 '0'가 아니면 text_arr에서 _를 지우고  slot_text에서 해당하는 태그에 단어를 담는다. 
       ##slot_text = {'abv': '', 'flavor': '', 'taste': '', 'types': ''}
     else:
@@ -138,25 +139,20 @@ def get_bot_response():
 
   # 메뉴판의 이름과 일치하는지 검증
   for k in app.slot_dict:
-  # k : 'types','abv','flavor','taste' 
       for x in dic[k]:
-      # {'types': [types], 'abv': [abv], 'flavor': [flavor], 'taste': [taste]}  
         x = x.lower().replace(" ", "\s*")
         m = re.search(x, slot_text[k])
         if m:
           app.slot_dict[k].append(m.group())
   print("app.slot_dict :", app.slot_dict)           
 
-  #options = {'beer_types':'종류', 'beer_abv':'도수', 'beer_flavor':'향', 'beer_taste':'맛'}
   empty_slot = [options[k] for k in app.slot_dict if not app.slot_dict[k]]
   filled_slot = [options[k] for k in app.slot_dict if app.slot_dict[k]]    
   print("empty_slot :", empty_slot)
   print("filled_slot :", filled_slot)
 
-  # for k in slot_text:
   uni_li = list(set([slot_text[k] for k in slot_text]))
   print(uni_li)
-  # if uni_li[0] == '':
   if ('종류' in empty_slot and '도수' in empty_slot and '향' in empty_slot and '맛' in empty_slot):
     message = '맥주의 종류, 도수, 향, 맛을 넣어서 다시 입력해주세요'   
   
@@ -173,16 +169,14 @@ def get_bot_response():
       message = "뭐찾니?"
     
     elif userText.strip().startswith("아니오"):
-      # last_msg = "바로 맥주 추천해줄게"
-      # return last_msg
       message = "바로 맥주 추천해줄게"
       init_app(app)
-            
+
   return message
 
 def catch_slot(i, inferred_tags, text_arr, slot_text):
   if not inferred_tags[0][i] == "O":
-    word_piece = re.sub("_", " ", text_arr[i])
+    word_piece = re.sub("_", "", text_arr[i])
     slot_text[inferred_tags[0][i]] += word_piece
 # inffered_tags = ['O', 'abv', 'abv', 'O', 'type', 'type', 'type', 'O', 'O', 'O', 'O', 'O', 'flavor', 'flavor', 'flavor', 'flavor', 'O', 'O']
 #text_arr = ['나는_', '7', '도_', '넘는_', '흑', '맥', '주로_', '주', '문', '하고_', '싶', '어_', '스', '모', '키', '한_', '걸', '로_']
@@ -206,7 +200,5 @@ def chatbot_msg(msg_li):
     
     msg_li = list(set(msg_li))
     message = f"너가 찾는 맥주 : {msg_li} <br />\n더 고려할 사항이 있니? (예 / 아니오)"
-    # message = "너가 찾는 맥주 : {} <br />\n더 고려할 사항이 있니? (예 / 아니오)".format(msg_li)
     
-    # ask_opt = True
     return message
